b34cd526917e50da007cc5365d47c954723c8a08
abdo-mo <abdo.mo.py3@gmail.com>
2021-05-21 22:32:13 +0200

    hide my secret key

--- dicussion_boart/settings.py
+++ dicussion_boart/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4s*#^ss#t@q292oj6b@e++fdl^=hv*ou4()a1ko-8u51=_tcij'
+SECRET_KEY = os.environ.get('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'boards_django',
         'USER': 'postgres',
-        'PASSWORD': 'testing321',
+        'PASSWORD': os.environ.get('PASS'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
