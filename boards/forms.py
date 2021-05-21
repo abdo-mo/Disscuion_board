a6d050b55f0087502d4af0f16ed732e6e3f0e89e
abdo-mo <abdo.mo.py3@gmail.com>
2021-05-21 15:17:42 +0200

    adding the main fils

---
+++ boards/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from . models import Topic
+
+class NewTopicForm(forms.ModelForm):
+    message = forms.CharField(widget = forms.Textarea(
+        attrs = {'rows':5, 'placeholder':'What in your mine?'}
+    ), max_length=4000,
+    help_text = 'the max length of the text is 4000')
+    class Meta:
+        model = Topic
+        fields = ['subject', 'message']
